14a76a354f759b2f44f74670078110374fa558eb
chenv <villon2chen@gmail.com>
2022-11-07 16:21:31 +0100

    Partially completed write command
    (write.c)
    Add file when there is not enough space will now overwrite the oldest file in the filesystem.
    TODO: Extract directories and file name from the input string
    
    (filesystem.c)
    File struct to return inode and bytes
    getFile to get the bytes from the file and generate an inode directly
    add_inode to add inode with every extra manipulation (like realloc)

--- src/filesystem.c
+++ src/filesystem.c
@@ -1,15 +1,20 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
+    unsigned long int id;       //TODO: remove it
     bool file;
-    unsigned long int nth;
-    unsigned long int size;
+    unsigned long int size;     // Size of the file associated to this inode 
     char name[64];
     char parent_directory[64];
-    // TODO: Date
 } Inode;
+
+typedef struct {
+    Inode inode;
+    char * bytes;
+} File;
 
 typedef struct {
     unsigned long int inode_number;
@@ -21,6 +26,7 @@ typedef struct {
     SuperBlock sb;
     Inode * inode_array;
     char ** bytes_array;
+    // Directory * directory_array;
 } FileSystem;
 
 FileSystem get_FS(char * path){
@@ -64,6 +70,66 @@ int put_FS(char * path, FileSystem fs){
     
     return 0;
 }
+
+File getFile(char * input_path, char * destination_path){
+    File file;
+
+    FILE * input_file;
+    input_file = fopen(input_path, "rb");
+    // TODO: Check if file exist
+
+    fseek(input_file, 0, SEEK_END);
+    unsigned long int input_file_size = ftell(input_file);
+    fseek(input_file, 0, SEEK_SET);
+
+    file.inode.file = true;
+    file.inode.size = input_file_size;
+    strcpy(file.inode.name, destination_path);  // Need to split the path
+    strcpy(file.inode.parent_directory, "/");   // Need to get the parent directory
+                                                // Need to create recursively parent direcory to the root
+
+    file.bytes = malloc(file.inode.size);
+    fread(file.bytes, input_file_size, 1, input_file);
+
+    fclose(input_file);
+
+    return file;
+}
+
+FileSystem add_inode(FileSystem fs, Inode inode, char * bytes){
+    inode.id = fs.sb.inode_number + 1;
+
+    fs.sb.inode_number += 1;
+    fs.sb.current_size += sizeof(Inode) + inode.size;
+
+    fs.inode_array = (Inode*)realloc(fs.inode_array, sizeof(Inode)*fs.sb.inode_number);
+    fs.bytes_array = (char **)realloc(fs.bytes_array, sizeof(char*)*fs.sb.inode_number);
+
+    fs.inode_array[fs.sb.inode_number-1] = inode;
+    fs.bytes_array[fs.sb.inode_number-1] = bytes;
+
+    return fs;
+}
+
+// FileSystem rm_inode(FileSystem fs, unsigned long int inode_id){
+//     fs.sb.inode_number -= 1;
+//     Inode * new_inode_array = malloc(sizeof(Inode)*fs.sb.inode_number);
+//     char ** new_bytes_array = malloc(sizeof(char*)*fs.sb.inode_number);
+
+
+//     for (unsigned long int i=inode_id-1; i<fs.sb.inode_number; i++){
+
+//         fs.inode_array[i] = fs.inode_array[i+1];
+//         fs.inode_array[i].id -= 1;
+
+//         fs.bytes_array[i] = malloc(fs.inode_array[i].size);
+//         fs.bytes_array[i] = fs.bytes_array[i+1];
+//     }
+
+//     fs.inode_array = (Inode*)realloc(fs.inode_array, sizeof(Inode)*fs.sb.inode_number);
+//     fs.bytes_array = (char **)realloc(fs.bytes_array, sizeof(char*)*fs.sb.inode_number);
+
+// }
 
 int free_FS(FileSystem fs){
     free(fs.bytes_array);
--- src/write.c
+++ src/write.c
@@ -1,35 +1,54 @@
 int myFS_write(char * input_path, char * destination_path){
     FileSystem fs = get_FS(PATH);
 
-    FILE * input_file;
-    input_file = fopen(input_path, "rb");
+    File file = getFile(input_path, destination_path);
 
-    fseek(input_file, 0, SEEK_END);
-    unsigned long int input_file_size = ftell(input_file);
-    fseek(input_file, 0, SEEK_SET);
+    // Check file size, if not ok, remove the oldest file
+    // To store a new file we need to check if the number of bytes of the file plus the size of one inode can fit
+    if (fs.sb.current_size + file.inode.size + sizeof(Inode) <= fs.sb.max_size ){
+        fs = add_inode(fs, file.inode, file.bytes);
+    } else {
+        if (fs.sb.inode_number == 1){
+            fs.inode_array[0] = file.inode;
+            fs.bytes_array[0] = file.bytes;
+        } else {
+            int i = 0; // i is the number of file to be removed
+            // temp_size is the size after adding the file
+            unsigned long int temp_size = fs.sb.current_size + file.inode.size + sizeof(Inode);
+            while (temp_size > fs.sb.max_size){
+                temp_size = temp_size - fs.inode_array[i].size;
+                i++;
+            }
 
-    Inode inode;
-    inode.file = true;
-    inode.nth = fs.sb.inode_number + 1;
-    inode.size = input_file_size;
-    strcpy(inode.name, destination_path);         // Need to split the path
-    strcpy(inode.parent_directory, "/");    // Need to get the parent directory
-                                            // Need to create recursively parent direcory to the root
+            fs.sb.current_size = temp_size;
+            fs.sb.inode_number = fs.sb.inode_number-i;
 
-    char * bytes = malloc(inode.size);
-    fread(bytes, input_file_size, 1, input_file);
+            for (int k=0; k<fs.sb.inode_number; k++){
+                fs.bytes_array[k] = fs.bytes_array[k+i];
+                fs.inode_array[k] = fs.inode_array[k+i];
+            }
 
-    fs.sb.inode_number += 1;
-    fs.sb.current_size += sizeof(Inode) + inode.size;
+            fs.inode_array = (Inode*)realloc(fs.inode_array, sizeof(Inode)*fs.sb.inode_number);
+            fs.bytes_array = (char **)realloc(fs.bytes_array, sizeof(char*)*fs.sb.inode_number);
 
-    fs.inode_array = (Inode*)realloc(fs.inode_array, sizeof(Inode)*fs.sb.inode_number);
-    fs.bytes_array = (char **)realloc(fs.bytes_array, sizeof(char*)*fs.sb.inode_number);
+            fs = add_inode(fs, file.inode, file.bytes);
+        }
 
-    fs.inode_array[fs.sb.inode_number-1] = inode;
-    fs.bytes_array[fs.sb.inode_number-1] = bytes;
+    }
 
     put_FS(PATH, fs);
 
+    int i = fs.sb.inode_number-1;
+    printf("Write file %s to filesystem %s:\n", input_path, PATH);
+    printf("-----------------------------------------------\n");
+    printf("Id: %ld\n", fs.inode_array[i].id);
+    printf("File: %d\n", fs.inode_array[i].file);
+    printf("Size: %ld B\n", fs.inode_array[i].size);
+    printf("Name: %s\n", fs.inode_array[i].name);
+    printf("Parent: %s\n", fs.inode_array[i].parent_directory);
+    printf("-----------------------------------------------\n");
+
+    free(file.bytes);
     free_FS(fs);
     return 0;
 }
